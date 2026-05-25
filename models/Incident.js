44e3ceae97d8ef56968f73fc7948d66e87c62f7e
Nirmalamangrati <157771329+Nirmalamangrati@users.noreply.github.com>
2026-05-25 21:18:56 +0545

    fixed sign up

--- models/Incident.js
+++ models/Incident.js
@@ -4,13 +4,31 @@ const IncidentSchema = new mongoose.Schema(
   {
     incidentCategory: { type: String, required: true }, // critical, high, medium, low
     incidentType: { type: String, required: true }, // fire, landslide, flood, other
+    incidentDate: { type: String, required: true }, // Mobile bata aako date string
     locationName: { type: String, required: true },
     latitude: { type: Number, required: true },
     longitude: { type: Number, required: true },
     description: { type: String, required: true },
-    status: { type: String, default: "pending" }, 
+    status: { type: String, default: "PENDING" }, // PENDING, APPROVED, RESOLVED
+    attachedFilePath: { type: String, default: "" }, // Attached image/file path
+
+    // 1. Suspect Info Model Array Setup:
+    suspectInfo: {
+      name: { type: String, default: "" },
+      age: { type: String, default: "" },
+      gender: { type: String, default: "" },
+      contact: { type: String, default: "" },
+    },
+
+    // 2. Reporter Info Data Setup:
+    reporterInfo: {
+      name: { type: String, default: "Anonymous" },
+      yourName: { type: String, default: "Anonymous" },
+      contact: { type: String, default: "" },
+      isAnonymous: { type: Boolean, default: false },
+    },
   },
-  { timestamps: true }, 
+  { timestamps: true },
 );
 
 module.exports = mongoose.model("Incident", IncidentSchema);
