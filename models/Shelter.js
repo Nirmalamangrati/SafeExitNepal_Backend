0ed9eec07fe294b808e76b030d0e3a09d7cba869
Nirmala Mangrati <nirmalamgrt@gmail.com>
2026-07-12 13:34:54 +0545

    force update HotlineModel casing inside controller

--- models/Shelter.js
+++ models/Shelter.js
@@ -15,7 +15,6 @@ const ShelterSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
-
 ShelterSchema.method("toJSON", function () {
   const { __v, _id, ...object } = this.toObject();
   object.id = _id;
